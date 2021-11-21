3faf6c9e7aa444d55a5f7e11d8b34cd94eb6f168
wangkang <wkang2015@163.com>
2021-11-21 21:27:25 +0800

    Optimise trace log about SSH
    (cherry picked from commit c5deca60dc0f24291906476c6bec623d1ff79a91)

--- .gitlab-ci.lib.sh
+++ .gitlab-ci.lib.sh
@@ -137,8 +137,18 @@ define_common_init_ssh() {
       do_print_info 'SSH ADD USER OK (same as default user)'
       return
     fi
-    [ -n "${ARG_SSH_KNOWN_HOSTS}" ] && _ssh_add_known "${ARG_SSH_KNOWN_HOSTS}"
-    [ -n "${ARG_SSH_PRIVATE_KEY}" ] && _ssh_add_key "${ARG_SSH_PRIVATE_KEY}"
+    local _pri_line
+    _pri_line=$(echo "${ARG_SSH_PRIVATE_KEY}" | tr -d '\n')
+    do_print_dash_pair 'Gitlab Custom Variables (ssh)'
+    do_print_dash_pair 'SSH_USER_HOST' "${_user_host}"
+    [ -n "${ARG_SSH_PRIVATE_KEY}" ] && (
+      do_print_dash_pair 'SSH_PRIVATE_KEY' "${_pri_line:0:60}**"
+      _ssh_add_key "${ARG_SSH_PRIVATE_KEY}"
+    )
+    [ -n "${ARG_SSH_KNOWN_HOSTS}" ] && (
+      do_print_dash_pair 'SSH_KNOWN_HOSTS' "${ARG_SSH_KNOWN_HOSTS:0:60}**"
+      _ssh_add_known "${ARG_SSH_KNOWN_HOSTS}"
+    )
     local _uid='-1'
     local _ssh="ssh ${SSH_DEBUG_OPTIONS}"
     _uid=$($_ssh "$_user_host" 'id') && do_print_info "SSH ADD USER OK ($_uid)"
@@ -181,12 +191,6 @@ define_common_init_ssh() {
     if [[ -z "${SSH_USER}" && -n "${SSH_USER_PREFIX}" ]]; then
       SSH_USER="${SSH_USER_PREFIX}-${ENV_NAME:?}"
     fi
-    _pri_line=$(echo "${SSH_PRIVATE_KEY}" | tr -d '\n')
-    do_print_dash_pair 'Gitlab Custom Variables (ssh)'
-    do_print_dash_pair 'SSH_USER' "${SSH_USER}"
-    do_print_dash_pair 'SSH_HOST' "${SSH_HOST}"
-    do_print_dash_pair 'SSH_PRIVATE_KEY' "${_pri_line:0:60}**"
-    do_print_dash_pair 'SSH_KNOWN_HOSTS' "${SSH_KNOWN_HOSTS:0:60}**"
     _ssh_agent_init
     _ssh_add_user_default
     do_ssh_vault_env_inject
