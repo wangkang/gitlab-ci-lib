ac94d911002024aaf07790c74f3159514bd40db8
wangkang <wkang2015@163.com>
2022-01-14 15:26:00 +0800

    Vault injection util bash and some tests

--- test/test.vault.sh
+++ test/test.vault.sh
@@ -1,19 +1,38 @@
-#!/bin/bash
-set -eo pipefail
-source "../.gitlab-ci.lib.sh"
+#!/bin/sh
+set -e
 
 export CUSTOMER="${VAULT_TEST_CUSTOMER:-missing}"
 export ENV_NAME='testing'
 
-define_common_init
-define_util_vault
+test_vault_gitlab_ci_lib() {
+  echo '# ----- test_vault_gitlab_ci_lib -----'
+  define_common_init
+  define_util_vault
+  do_vault_service_env_file 'dummy-env'
+  do_vault_service_patch_file 'dummy-alpine-etc' 'config.yml'
+  export CI_PROJECT_NAME='dummy-service'
+  export OPTION_DEBUG='yes'
+  init_inject_env_bash_do
+  init_inject_ci_bash_do
+  init_inject_cd_bash_do
+  export OPTION_DEBUG='no'
+}
+. "../.gitlab-ci.lib.sh"
+test_vault_gitlab_ci_lib
 
-do_vault_service_env_file 'dummy-env'
-do_vault_service_patch_file 'dummy-alpine-etc' 'config.yml'
-
-export CI_PROJECT_NAME='dummy-service'
-export OPTION_DEBUG='yes'
-init_inject_env_bash_do
-init_inject_ci_bash_do
-init_inject_cd_bash_do
-export OPTION_DEBUG='no'
+test_vault_injection() {
+  echo '# ----- test_vault_injection begin -----'
+  echo '# ----- do_vault_login -----'
+  # shellcheck disable=SC2155
+  export VAULT_TOKEN="$(do_vault_login)"
+  [ -z "${VAULT_TOKEN}" ] && exit 0
+  echo '# ----- do_vault_fetch -----'
+  do_vault_fetch "dummy-env"
+  # Do not reuse VAULT_TOKEN: VAULT_TOKEN=''
+  echo '# ----- do_vault_export -----'
+  do_vault_export "dummy-env"
+  echo '# ----- test_vault_injection end -----'
+}
+export VAULT_PATH="${CUSTOMER}-${ENV_NAME}/data/service"
+. "../util/vault-injection.sh"
+test_vault_injection
---
+++ util/vault-injection.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+set -e
+do_vault_login() {
+  if [ -n "${VAULT_TOKEN}" ]; then
+    printf '%s' "${VAULT_TOKEN}"
+    return
+  fi
+  echo "# vault login: '${VAULT_URL}' -- $(date)" >&2
+  if [ -z "$(command -v jq)" ]; then
+    echo '##jq is not installed' >&2
+    return
+  fi
+  if [ -z "${VAULT_URL}" ]; then
+    echo "##env variable 'VAULT_URL' is absent" >&2
+    return
+  fi
+  if [ -z "${VAULT_USER}" ]; then
+    echo "##env variable 'VAULT_USER' is absent" >&2
+    return
+  fi
+  if [ -z "${VAULT_PASS}" ]; then
+    echo "# env variable 'VAULT_PASS' is absent" >&2
+    if [ -z "${VAULT_PASS_SYS}" ]; then
+      echo "##env variable 'VAULT_PASS_SYS' is absent" >&2
+      return
+    fi
+  fi
+  _json="{\"password\":\"${VAULT_PASS:-${VAULT_PASS_SYS:?}}\"}"
+  _url_login="${VAULT_URL:?}/auth/userpass/login/${VAULT_USER:?}"
+  _token=$(curl -s --max-time 5 --request POST "${_url_login}" --data "${_json}" | jq -r '.auth.client_token')
+  if [ -z "${_token}" ]; then
+    echo '##vault login failed' >&2
+    return
+  fi
+  export VAULT_TOKEN="${_token}"
+  echo "# vault login token: '$(echo "${_token}" | cut -c 1-8) *** ***'" >&2
+  printf '%s' "${_token}"
+}
+do_vault_fetch() {
+  _path0="${1}"
+  if [ '/' = "$(echo "${_path0:?}" | cut -c 1)" ]; then
+    _path="${_path0}"
+  else
+    # shellcheck disable=SC2153
+    _path="${VAULT_PATH}/${_path0}"
+  fi
+  [ '/' != "$(echo "${_path}" | cut -c 1)" ] && _path="/${_path}"
+  _token="$(do_vault_login)"
+  if [ -z "${_token}" ]; then return; fi
+  _from_url="${VAULT_URL:?}${_path}"
+  echo "# vault fetch: ${_from_url:?} -- $(date)" >&2
+  set +e
+  curl -s --max-time 5 "${_from_url:?}" -H "X-Vault-Token: ${_token:?}" 2>/dev/null
+  echo "# vault fetch exit status $?" >&2
+  set -e
+}
+do_vault_export() {
+  echo "# vault export path: '${1}' -- $(date)"
+  _jq_cmd='.data.data|select(.!=null)|to_entries[]|"export \(.key)=\"\(.value)\";"'
+  _code="$(do_vault_fetch "${1}" | jq -r "${_jq_cmd}")"
+  echo "# vault export ${#_code} char(s)"
+  if [ -n "${_code}" ]; then eval "${_code}"; fi
+  echo "# vault export finished -- $(date)"
+}
